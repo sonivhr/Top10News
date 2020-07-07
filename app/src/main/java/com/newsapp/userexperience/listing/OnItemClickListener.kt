d2c53e3976fec141e9e3402d1cb6bbc34d64a369
vishalsoni <sonivhr@gmail.com>
2020-07-07 19:17:16 +0530

    Removed unused code and screens.

--- app/src/main/java/com/newsapp/userexperience/listing/OnItemClickListener.kt
+++
@@ -1,7 +0,0 @@
-package com.newsapp.userexperience.listing
-
-import com.newsapp.rest.response.StoryDetails
-
-interface OnItemClickListener {
-    fun onItemClick(position: Int, storyDetails: StoryDetails)
-}
