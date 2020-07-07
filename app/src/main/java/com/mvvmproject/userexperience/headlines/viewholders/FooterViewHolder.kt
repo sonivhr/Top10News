81e5d0e5b6a5a02094804b2095ce20f6efeba721
vishalsoni <sonivhr@gmail.com>
2020-07-07 13:25:47 +0530

    Use correct Exception class for connectivity issue.

--- app/src/main/java/com/mvvmproject/userexperience/headlines/viewholders/FooterViewHolder.kt
+++ app/src/main/java/com/mvvmproject/userexperience/headlines/viewholders/FooterViewHolder.kt
@@ -1,10 +1,10 @@
 package com.mvvmproject.userexperience.headlines.viewholders
 
-import android.accounts.NetworkErrorException
 import androidx.recyclerview.widget.RecyclerView
 import com.mvvmproject.R
 import com.mvvmproject.databinding.ListItemFooterBinding
 import com.mvvmproject.helperclasses.DataLoadingState
+import com.mvvmproject.helperclasses.NoInternetException
 
 class FooterViewHolder(private val listItemFooterBinding: ListItemFooterBinding) :
     RecyclerView.ViewHolder(listItemFooterBinding.root) {
@@ -13,7 +13,7 @@ class FooterViewHolder(private val listItemFooterBinding: ListItemFooterBinding)
         listItemFooterBinding.dataLoadingState = dataLoadingState
         if (dataLoadingState is DataLoadingState.Error) {
             listItemFooterBinding.tvErrorMessage.text =
-                if (dataLoadingState.throwable is NetworkErrorException) {
+                if (dataLoadingState.throwable is NoInternetException) {
                     listItemFooterBinding.root.context.getString(R.string.message_no_connectivity)
                 } else {
                     listItemFooterBinding.root.context.getString(R.string.message_issue_while_loading_data)
